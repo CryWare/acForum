d7f73779accae97927a99de9bf94e29db1c0c9ae
Marcel Brand <marcel.brand@achtachtel.de>
2013-03-05 16:36:14 +0100

    added config file

---
+++ config/module.config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * acForum (http://github.com/achtachtel/acForum/)
+ *
+ * @copyright Copyright 2013 Marcel Brand, Patric Mahler
+ * @license GPLv3 http://gnu.org/licenses/gpl.html
+ * @package acForum
+ */
+
+return array();
